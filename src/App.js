987a53a5d6aeaf5e4c340cc46fe84a6b1d73cf86
Kevin Nguyen <kevinnguyen@Kevins-MacBook-Air.local>
2021-11-07 23:12:24 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from '@testing-library/react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './app.css';
+
+export default function App() {
+    let navigate = useNavigate();
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    
+    // this can be used to render the next page component or something!
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+    }
+
+    return (
+        <div>
+            <h1 className="helloWorld">Hello World!</h1>
+            <p className="count">You clicked <strong>{count}</strong> times. <br />
+            Click 10 times to unlock the username and password :)</p>
+            <button className="count" onClick={() => {
+                setCount(count+1)
+                if(count === 9) {   //idk why its 9
+                    render(
+                        <div> 
+                            {/* need to workout how to unrender this... */}
+                            <p className="login">Username: batman<br />
+                            Password: alfred</p>
+                        </div>
+                    )
+                }
+                }}>Click</button>
+            <form className="login" onSubmit={handleSubmit}>
+                Username: <input type="text" name="username" onChange={e => setName(e.target.value)} autocomplete="off"/><br />
+                Password: <input type="password" name="password" onChange={s => setPassword(s.target.value)} autocomplete="off" /><br />
+                <input type="button" value="Submit" onClick={() => {
+                    if(name === "batman" && password === "alfred") {
+                        navigate("/batcave");
+                    }
+                }} />
+            </form>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/batcave.js
@@ -0,0 +1,9 @@
+import "./batcave.css";
+
+export default function Batcave() {
+    return (
+        <div className="batcave">
+            <h1 className="greeting">Welcome back Sir</h1>
+        </div>
+    );
+}
